1e272b62906b7a2305a345cb8fa56323f9046c35
James Stephenson <c4p7.fl1n7@gmail.com>
2018-12-24 19:44:35 -0500

    Making my own snow on xmas eve.

--- lcd.c
+++ lcd.c
@@ -1,8 +1,18 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 SDL_Window *lcddisp;
 SDL_Renderer *dispRend;
+
+unsigned char randomPixel() {
+	long int ri;
+	unsigned char rpixel;
+	ri = random();
+	rpixel = (unsigned char)ri;
+	rpixel = rpixel & 3;
+	return rpixel;
+}
 
 void create_display(int scale) {
 	lcddisp = SDL_CreateWindow(
@@ -19,19 +29,16 @@ void create_display(int scale) {
 void main() {
 	int scale;
 	int time;
-	char pixels[160 * 144];
+	unsigned char pixels[160 * 144];
 	int pixelIndex;
-	char curPixel;
-	int reps;
+	int framesRendered;
 	SDL_Surface *origScreen;
-	SDL_Surface *scaledScreen;
 	SDL_Texture *screenTexture;
 	SDL_Palette *lcdPalette;
 	SDL_Color pixelColors[4];
 	pixelIndex = 0;
-	curPixel = 0;
-	reps = 1;
 	scale = 4;
+	framesRendered = 0;
 	pixelColors[0] = (SDL_Color){ 255, 255, 255, 255 };
 	pixelColors[1] = (SDL_Color){ 170, 170, 170, 255 };
 	pixelColors[2] = (SDL_Color){ 85, 85, 85, 255 };
@@ -40,40 +47,28 @@ void main() {
 	SDL_SetPaletteColors(lcdPalette, pixelColors, 0, 4);
 	SDL_Init(SDL_INIT_VIDEO);
 	create_display(scale);
-	while (pixelIndex++ < (160 * 144)) {
-		pixels[pixelIndex] = curPixel;
-		if (++reps == 8) {
-			reps = 0;
-			if (++curPixel == 4)
-				curPixel = 0;
-		}
-	}
-	pixelIndex = 0;
-	scaledScreen = SDL_CreateRGBSurfaceWithFormat(
-			0,
-			160 * scale,
-			144 * scale,
-			8,
-			SDL_PIXELFORMAT_INDEX8);
-	origScreen = SDL_CreateRGBSurfaceWithFormatFrom(
-			pixels,
-			160,
-			140,
-			8,
-			160,
-			SDL_PIXELFORMAT_INDEX8);
-	SDL_SetSurfacePalette(origScreen, lcdPalette);
-	screenTexture = SDL_CreateTextureFromSurface(
-			dispRend,
-			origScreen);
-	SDL_RenderCopy(dispRend, screenTexture, NULL, NULL);
-	//SDL_SetRenderDrawColor(dispRend, 255, 0, 0, 255);
-	//SDL_RenderClear(dispRend);
-	SDL_RenderPresent(dispRend);
 	time = SDL_GetTicks();
-	SDL_Delay(3000);
+	while (framesRendered++ < 600) {
+		while (pixelIndex < (160 * 144)) {
+			pixels[pixelIndex++] = randomPixel();
+		}
+		pixelIndex = 0;
+		origScreen = SDL_CreateRGBSurfaceWithFormatFrom(
+				pixels,
+				160,
+				140,
+				8,
+				160,
+				SDL_PIXELFORMAT_INDEX8);
+		SDL_SetSurfacePalette(origScreen, lcdPalette);
+		screenTexture = SDL_CreateTextureFromSurface(
+				dispRend,
+				origScreen);
+		SDL_RenderCopy(dispRend, screenTexture, NULL, NULL);
+		SDL_RenderPresent(dispRend);
+	}
 	time = SDL_GetTicks() - time;
 	SDL_Quit();
-	printf("%d\n", time);
+	printf("%d frames rendered in %d ms.\n", framesRendered - 1, time);
 }
 
